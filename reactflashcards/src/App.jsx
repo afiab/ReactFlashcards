efd2a91d4e8b438112eae3c28934ff07b7890e19
afiab <abidica30@stuy.edu>
2024-03-09 01:53:35 -0500

    implemented most of the required features, updated readme

--- reactflashcards/src/App.jsx
+++ reactflashcards/src/App.jsx
@@ -1,34 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Flashcard from './component/Flashcard'
+import Button from './component/Button'
+import French from './component/french.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [index, setIndex] = useState(0);
+  const [card, setCard] = useState(French[0]);
+
+  function prevCard() {
+    setIndex(prevIndex => prevIndex-1)
+  }
+
+  function nextCard() {
+    setIndex(nextIndex => nextIndex+1)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="head">
+        <h1>You think you can speak French?</h1>
+        <h2>Examinez vos capacités ici!</h2>
+        <h2>Number of cards: {French.length}</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Flashcard 
+        {...French[index]}
+      />
+      <Button onClick={prevCard} disabled = {index === 0 ? true:false}>&larr;</Button>
+      <Button onClick={nextCard} disabled = {index === French.length - 1 ? true:false}>&rarr;</Button>
+    </div>    
   )
 }
 
